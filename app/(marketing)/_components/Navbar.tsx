4f61955dda110b7653905b5b26fc29c31a48f016
esen13 <esen.arykbaev@gmail.com>
2023-11-30 21:25:47 +0600

    trello app v0.0.2

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,8 +1,14 @@
+import React from "react";
 import {Logo} from "@/components/logo";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
+import { useUser } from "@clerk/nextjs";
 
 export const Navbar = () => {
+
+    // const { isSignedIn, user } = useUser();
+    // console.log('isSignedIn', isSignedIn)
+
     return (
         <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm
         bg-white flex items-center">
@@ -11,16 +17,22 @@ export const Navbar = () => {
                 <Logo />
                 <div className="space-x-4 md:block md:w-auto flex
                 items-center justify-between w-full">
-                    <Button size="sm" variant="outline" asChild>
-                        <Link href="/sign-in">
-                            Sign in
-                        </Link>
-                    </Button>
-                    <Button size="sm" variant="default" asChild>
-                        <Link href="/sign-up">
-                            Get Taskify
-                        </Link>
-                    </Button>
+                    {/*{!isSignedIn ? (<>*/}
+                        <Button size="sm" variant="outline" asChild>
+                            <Link href="/sign-in">
+                                Sign in
+                            </Link>
+                        </Button>
+                        <Button size="sm" variant="default" asChild>
+                            <Link href="/sign-up">
+                                Get Taskify
+                            </Link>
+                        </Button>
+                    {/*</>) : (*/}
+                    {/*    <div>*/}
+                    {/*        {user.fullName}*/}
+                    {/*    </div>*/}
+                    {/*)}*/}
                 </div>
             </div>
         </div>
---
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -0,0 +1,13 @@
+const OrganizationLayout = ({
+                             children
+                         }: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="h-full">
+            {children}
+        </div>
+    );
+};
+
+export default OrganizationLayout;
